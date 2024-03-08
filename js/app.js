66fb81154ef31eac3bc40ae43e821458066ad9c3
yasin <wolfsuper474@gmail.com>
2024-03-08 11:31:02 +0330

    date project

---
+++ js/app.js
@@ -0,0 +1,81 @@
+let userdate = prompt("say your date")
+
+for(let i = 0 ; i <=100 ; i++ ){
+    if(userdate == i){
+        alert("century = 1")
+    }
+}
+for(let i = 101 ; i <=200 ; i++ ){
+    if(userdate == i){
+        alert("century = 2")
+    }
+}
+for(let i = 201 ; i <=300 ; i++ ){
+    if(userdate == i){
+        alert("century = 3")
+    }
+}
+for(let i = 301 ; i <=400 ; i++ ){
+    if(userdate == i){
+        alert("century = 4")
+    }
+}
+for(let i = 401 ; i <=500 ; i++ ){
+    if(userdate == i){
+        alert("century = 5")
+    }
+}
+for(let i = 501 ; i <=600 ; i++ ){
+    if(userdate == i){
+        alert("century = 6")
+    }
+}
+for(let i = 601 ; i <=700 ; i++ ){
+    if(userdate == i){
+        alert("century = 7")
+    }
+}
+for(let i = 701 ; i <=800 ; i++ ){
+    if(userdate == i){
+        alert("century = 8")
+    }
+}
+for(let i = 801 ; i <=900 ; i++ ){
+    if(userdate == i){
+        alert("century = 9")
+    }
+}
+for(let i = 901 ; i <=1000 ; i++ ){
+    if(userdate == i){
+        alert("century = 10")
+    }
+}
+for(let i = 1001 ; i <=1100 ; i++ ){
+    if(userdate == i){
+        alert("century = 11")
+    }
+}
+for(let i = 1101 ; i <=1200 ; i++ ){
+    if(userdate == i){
+        alert("century = 12")
+    }
+}
+for(let i = 1201 ; i <=1300 ; i++ ){
+    if(userdate == i){
+        alert("century = 13")
+    }
+}
+for(let i = 1301 ; i <=1400 ; i++ ){
+    if(userdate == i){
+        alert("century = 14")
+    }
+}
+for(let i = 1401 ; i <=1500 ; i++ ){
+    if(userdate == i){
+        alert("century = 15")
+    }
+}
+
+
+
+
